d6d67b6ba3314aa2d5821de99527eb302a62605e
marceloflp <mfelipedasilva2020@gmail.com>
2024-06-02 22:23:18 -0300

    Criando UsuarioController

---
+++ src/main/java/com/projetomf/cadastroUsuarios/controllers/UsuarioController.java
@@ -0,0 +1,11 @@
+package com.projetomf.cadastroUsuarios.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioController {
+
+	
+}

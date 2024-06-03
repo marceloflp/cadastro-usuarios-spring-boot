db15694794b17193692cadd77797a0dc36e4a3bd
marceloflp <mfelipedasilva2020@gmail.com>
2024-06-03 15:36:02 -0300

    Implementação do CRUD

--- src/main/java/com/projetomf/cadastroUsuarios/controllers/UsuarioController.java
+++ src/main/java/com/projetomf/cadastroUsuarios/controllers/UsuarioController.java
@@ -1,43 +1,59 @@
 package com.projetomf.cadastroUsuarios.controllers;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetomf.cadastroUsuarios.entities.Usuario;
+import com.projetomf.cadastroUsuarios.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioController {
 
-	//Criando findAll sem a lógica correta
+	@Autowired
+	private UsuarioService service;
+	
+	@GetMapping
 	public ResponseEntity<List<Usuario>> findAll(){
-		List<Usuario> list = new ArrayList<>();
+		List<Usuario> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
-	//Criando findById sem a lógica correta
-	public ResponseEntity<Usuario> findById(Long id){
-		Usuario user = new Usuario();
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+		Usuario user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 	
-	//Criando insert sem a lógica correta
-	public ResponseEntity<Usuario> insert(){
-		Usuario user = new Usuario();
-		return ResponseEntity.ok().body(user);
+	@PostMapping
+	public ResponseEntity<Usuario> insert(@RequestBody Usuario obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();		
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
-	//Criando delete sem a lógica correta
-	public ResponseEntity<Void> delete(Long id){
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
-	//Criando update sem a lógica correta
-	public ResponseEntity<Usuario> update(Long id, Usuario user){
-		return ResponseEntity.ok().body(user);
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Usuario obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/projetomf/cadastroUsuarios/services/UsuarioService.java
+++ src/main/java/com/projetomf/cadastroUsuarios/services/UsuarioService.java
@@ -1,6 +1,46 @@
 package com.projetomf.cadastroUsuarios.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetomf.cadastroUsuarios.entities.Usuario;
+import com.projetomf.cadastroUsuarios.repositories.UsuarioRepository;
+
+@Service
 public class UsuarioService {
-	
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public List<Usuario>findAll() {
+		return repository.findAll();
+	}
+
+	public Usuario findById(Long id) {
+		Optional<Usuario> obj =repository.findById(id);
+		return obj.get();
+	}
+
+	public Usuario insert(Usuario obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	public Usuario update(Long id, Usuario obj) {
+		Usuario entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Usuario entity, Usuario obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());	
+	}
 
 }
